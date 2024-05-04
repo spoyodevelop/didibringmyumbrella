8eef2eb0c8ae9ee4884fbf5d6ad47401df32a121
spoyodevelop <kjy9505@gmail.com>
2024-05-05 04:40:13 +0900

    added mobile responsive design

--- components/ui/CloseButton.js
+++ components/ui/CloseButton.js
@@ -6,7 +6,7 @@ const CloseButton = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-4 h-4 md:w-6 md:h-6"
     >
       <path
         strokeLinecap="round"
--- components/ui/Notification.js
+++ components/ui/Notification.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useWeatherStore } from "@/app/store/weather-store";
 import CloseButton from "./CloseButton";
+import styles from "./Notification.module.css";
 
 const Notification = () => {
   const {
@@ -12,12 +13,16 @@ const Notification = () => {
   } = useWeatherStore();
 
   const [showNotification, setShowNotification] = useState(false);
-
+  const colorVariants = {
+    success: `alert alert-success fixed bottom-0 right-0 m-4 z-50 w-auto p-4 rounded-md shadow-md transition-opacity ease-in duration-700 opacity-100`,
+    error: `alert alert-error fixed bottom-0 right-0 m-4 z-50 w-auto p-4 rounded-md shadow-md transition-opacity ease-in duration-700 opacity-100`,
+  };
   useEffect(() => {
     let timer;
     let time;
     if (systemMessage) {
       setShowNotification(true);
+
       if (systemStatus === "success") {
         time = 2000;
       } else if (systemStatus === "error") {
@@ -36,16 +41,16 @@ const Notification = () => {
   return (
     <div>
       {showNotification && (
-        <div
-          className={`alert alert-${systemStatus} fixed bottom-0 right-0 m-4 z-50 w-auto p-4 rounded-md shadow-md transition-opacity ease-in duration-700 opacity-100 hover:opacity-0`}
-        >
+        <div className={`${colorVariants[systemStatus]}`}>
           {systemMessage}
-          <button
-            className={`btn btn-outline btn-circle btn-sm btn-light hover:bg-gray-100 hover:text-gray-800`}
-            onClick={() => setShowNotification(false)}
-          >
-            <CloseButton />
-          </button>
+          <div className="hidden md:block">
+            <button
+              className={`btn btn-outline btn-circle btn-sm btn-light hover:bg-gray-100 hover:text-gray-800 `}
+              onClick={() => setShowNotification(false)}
+            >
+              <CloseButton />
+            </button>
+          </div>
         </div>
       )}
     </div>
