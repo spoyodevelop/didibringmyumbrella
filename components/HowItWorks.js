ffdf20a6a365e0bf1e830d46eaf9eb35ca954de9
spoyodevelop <kjy9505@gmail.com>
2024-05-15 20:28:36 +0900

    another minor changes

--- components/HowItWorks.js
+++ components/HowItWorks.js
@@ -6,9 +6,9 @@ const HowItWorks = ({ className }) => {
   return (
     <section className={className}>
       <div className="w-full shadow-xl lg:w-3/4 bg-slate-200 rounded-xl">
-        <section className="flex flex-col p-8">
-          <h1 className="text-xl lg:text-2xl">
-            실제 강수확률과 비교해보세요. 사용방법은 간단합니다.
+        <section className="flex flex-col p-2 py-8 md:p-8">
+          <h1 className="text-xl text-center lg:text-2xl md:text-start">
+            사용방법은 간단합니다.
           </h1>
           <Steps className={className} />
         </section>
