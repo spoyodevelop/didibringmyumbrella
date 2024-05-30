45fc564c926c5b94c6e97bcac538f3edf583ad6b
spoyodevelop <kjy9505@gmail.com>
2024-05-30 16:01:02 +0900

    done

--- app/layout.js
+++ app/layout.js
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
           >
             <SWRProvider>{children}</SWRProvider>
           </main>
+          <SpeedInsights />
         </SmoothScrolling>
       </body>
     </html>
